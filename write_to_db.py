4b1772130cb35f8fee2b89e31cf8bba2d37499f5
Diogo Nunes <diogovaznunes@gmail.com>
2015-06-03 00:28:03 +0100

    Working db script

--- write_to_db.py
+++ write_to_db.py
@@ -9,24 +9,27 @@ con = None
 try:
     con = sqlite3.connect('./proteinDatabase.db')
 
-    with open('teste.json') as data_file:
+    with open('result.json') as data_file:
         data = json.load(data_file)
         with con:
             cur = con.cursor()
             for gene in data:
-                cur.execute("INSERT OR IGNORE INTO Genes VALUES(?, ?, ?, ?, ?)", (gene['gene'], gene['organism'] , gene['three_prime'] , time.strftime("%H:%M:%S") , time.strftime("%H:%M:%S")))
+                #print json.dumps(gene, indent=4, sort_keys=True)
+                cur.execute("INSERT OR IGNORE INTO Genes VALUES(?, ?, ?, ?, ?)", (gene['gene'], gene.get('organism') , gene.get('three_prime') , time.strftime("%H:%M:%S") , time.strftime("%H:%M:%S")))
                 for transcripts in gene['transcripts']:
-                    print transcripts['protein']['interactions']
-                    cur.execute("INSERT OR IGNORE INTO Proteins VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" ,
-                                (transcripts['protein']['name'], transcripts['protein']['uniprot_url'], transcripts['protein']['pdb_url'],
-                                 transcripts['protein']['names_and_taxonomy']['taxonomic_lineage'], transcripts['protein']['names_and_taxonomy']['organism'],
-                                 transcripts['protein']['names_and_taxonomy']['protein_names'], transcripts['protein']['names_and_taxonomy']['taxonomic_identifier'],
-                                 transcripts['protein']['names_and_taxonomy']['proteomes'][0], transcripts['protein']['interactions'],
-                                 transcripts['protein']['keywords_molecular_function'], transcripts['protein']['keywords_ligand'],
-                                 transcripts['protein']['keywords_biological_process'], time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S")))
+                    #print json.dumps(transcripts, indent=4, sort_keys=True)
+                    #print transcripts['protein'].get('name')
                     cur.execute("INSERT OR IGNORE INTO Transcripts VALUES(?, ?, ?, ?)", (transcripts['transcript'],transcripts['transcript_url'], time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S")))
-                    cur.execute("INSERT OR IGNORE INTO GeneTranscript VALUES(?, ?, ?, ?)", (time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S"), transcripts['name'], transcripts['transcript']))
-                    cur.execute("INSERT OR IGNORE INTO TranscriptProtein VALUES(?,?,?,?)", (time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S"), gene['gene'], transcripts['transcript']))
+                    if transcripts['protein'].get('name'):
+                        cur.execute("INSERT OR IGNORE INTO Proteins VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" ,
+                        (transcripts['protein']['name'], transcripts['protein']['uniprot_url'], transcripts['protein']['pdb_url'],
+                         transcripts['protein']['names_and_taxonomy']['taxonomic_lineage'], transcripts['protein']['names_and_taxonomy']['organism'],
+                         transcripts['protein']['names_and_taxonomy']['protein_names'], transcripts['protein']['names_and_taxonomy']['taxonomic_identifier'],
+                         transcripts['protein']['names_and_taxonomy']['proteomes'][0], transcripts['protein']['interactions'],
+                         transcripts['protein']['keywords_molecular_function'], transcripts['protein']['keywords_ligand'],
+                         transcripts['protein']['keywords_biological_process'], time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S")))
+                        cur.execute("INSERT OR IGNORE INTO GeneTranscript VALUES(?, ?, ?, ?)", (time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S"), transcripts['transcript'], gene['gene']))
+                        cur.execute("INSERT OR IGNORE INTO TranscriptProtein VALUES(?,?,?,?)", (time.strftime("%H:%M:%S"), time.strftime("%H:%M:%S"), transcripts['protein']['name'], transcripts['transcript']))
 
 
 
